95863c6fd02d3d74c6899cdd5ec1475270a4d8d2
manato miura <mmiura2351@gmail.com>
2023-10-17 09:36:02 +0900

    formatting

--- src/components/format.tsx
+++ src/components/format.tsx
@@ -1,11 +1,5 @@
-import styles from "../styles/Home.module.css";
-
 const Format = () => {
-	return (
-		<>
-			<div className={styles.template}>format</div>
-		</>
-	);
+	return <>format</>;
 };
 
 export default Format;
